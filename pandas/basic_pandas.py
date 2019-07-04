556bb2331a679cb40eb101181556766e0dd1b410
adair_chan <adairchan.dream@gmail.com>
2019-07-04 16:06:25 +0800

    更新pandas的选择，赋值， 缺失值等基础示例操作

--- pandas/basic_pandas.py
+++ pandas/basic_pandas.py
@@ -33,15 +33,15 @@ df1.tail(3)
 # 显示索引、列和底层NumPy数据
 print(df1.index)
 print(df1.columns)
-print(df2.values)
+print(df1.values)
 # describe() 方法显示数据的快速统计摘要
-df2.describe()
+df1.describe()
 # 转置
 df1.T
 # 按轴排序
 df1.sort_index(axis=1, ascending=False)
 # 按值排序
-df1.sort_index(by="B")
+df1.sort_values(by="B")
 
 
 """
@@ -54,7 +54,7 @@ df1.loc[dates[0]]
 # 通过标签在多个轴上选择数据
 df1.loc[:, ['A', 'B']]
 # 通过标签同时在两个轴上切片
-df1.loc['20130102':'20130104', ['A', 'B']]
+df1.loc['20190702':'20190704', ['A', 'B']]
 
 # 按位置选择
 print(df1.iloc[3])
@@ -66,4 +66,30 @@ df3 = df1.copy()
 df3['E'] = ['one', 'one', 'two', 'three', 'four', 'three']
 print(df3[df3['E'].isin(['two', 'four'])])
 
+"""
+赋值
+"""
+# 添加新列将自动根据索引对齐数据
+s1 = pd.Series([1, 2, 3, 4, 5, 6], index=pd.date_range('20190701', periods=6))
+df1['F'] = s1
+# 通过标签赋值
+df1.at[dates[0], 'A'] = 0
+# 通过位置赋值
+df1.at[dates[0], 'A'] = 0
+# 通过位置赋值
+df1.loc[:, 'D'] = np.array([5] * len(df1))
+
+
+"""
+缺失值
+"""
+# 重建索引允许你更改/添加/删除指定轴上的索引。 这个操作会返回一个副本(不会更改原来的对象)
+df4 = df1.reindex(index=dates[0:4], columns=list(df1.columns) + ['E'])
+df4.loc[dates[0]:dates[1], 'E'] = 1
+# 删除任何带有缺失值的行
+df4.dropna(how='any')
+# 填充缺失值
+df4.fillna(value=5)
+# 获取值为na的掩码(True/False)
+pd.isna(df4)
 
