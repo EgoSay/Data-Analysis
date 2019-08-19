4c413243721678c7956cae98f788a3b1c476591b
adair_chan <adairchan.dream@gmail.com>
2019-08-19 11:48:55 +0800

    update

---
+++ pandasLearning/pandas_example2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Author  : adair_chan
+# @Email   : adairchan.dream@gmail.com
+# @Date    : 2019/7/9 下午12:14
+# @IDE     : PyCharm
+
+import pandas as pd
+import numpy as np
+from pandasLearning.data import data_df, result_rdd, result_dict
+
+pd_data = pd.DataFrame(data_df)
+print(pd_data)
