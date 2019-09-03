89c8ea0df43072bc68bda80dceb2bd5561049d5d
adair_chan <adairchan.dream@gmail.com>
2019-09-03 16:03:09 +0800

    Remove the now ignored things

--- DataExploration/data_feature_analysis/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3.6
-# -*- coding: utf-8 -*-
-# @Author  : adair_chan
-# @Email   : adairchan.dream@gmail.com
-# @Date    : 2019/8/27 下午8:46
-# @IDE     : PyCharm
--- DataExploration/data_quality_analysis/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3.6
-# -*- coding: utf-8 -*-
-# @Author  : adair_chan
-# @Email   : adairchan.dream@gmail.com
-# @Date    : 2019/8/20 下午4:55
-# @IDE     : PyCharm
